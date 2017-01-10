3e797015ff2660197255af7aa82ae62c5d533b5f
Grace <grace.chinhanyu@gmail.com>
2017-01-10 22:54:43 +0800

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -30,13 +30,33 @@ io.on('connection', (socket) => {
 	// 	console.log('createEmail', newEmail);
 	// });
 
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat app',
+		createdAt: new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined',
+		createdAt: new Date().getTime()
+	});
+
+
 	socket.on('createMessage', (newMessage) => {
 		console.log('createMessage', newMessage);
-		io.emit('newMessage', {
+		// io.emit('newMessage', {
+		// 	from: newMessage.from,
+		// 	text: newMessage.text,
+		// 	createdAt: new Date().getTime()
+		// });
+
+		socket.broadcast.emit('newMessage', {
 			from: newMessage.from,
 			text: newMessage.text,
 			createdAt: new Date().getTime()
 		});
+
 	});
 
 	socket.on('disconnect', () => {
